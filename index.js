528afa43ba45d04d537e2c86b45d3f573f051ffd
dzmitristasenko <dzmitri.stasenko@startmatter.com>
2019-09-18 20:08:12 +0300

    Connect MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 express()
   .get('/', (req, res) => res.send('ok'))
