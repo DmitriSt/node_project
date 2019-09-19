499c26fb53416f3a1ddab4e1cfde12601913a1b2
dzmitristasenko <dzmitri.stasenko@startmatter.com>
2019-09-19 19:01:51 +0300

    Create CRUD

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const config = require('config');
+const app = express();
+app.use(express.json());
+
+const Animal = require('./models/Animal');
 
 const db = config.get('mongoURI');
 
@@ -11,6 +14,50 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-express()
-  .get('/', (req, res) => res.send('ok'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const Panda = new Animal({
+  name: 'Red Panda',
+  isEndangered: true
+});
+const Tiger = new Animal({
+  name: 'White Tiger',
+  isEndangered: false
+});
+const Bear = new Animal({
+  name: 'Yellow Bear',
+  isEndangered: false
+});
+
+// Read all entries
+app.get('/', (req, res) => {
+  Animal.find()
+    .sort({ date: -1 })
+    .then(items => console.log(res.json(items)));
+});
+
+// Add a new entry
+app.post('/', (req, res) => {
+  const newAnimal = new Animal({
+    name: req.body.name,
+    isEndangered: req.body.isEndangered || false,
+  });
+  newAnimal
+    .save()
+    .then(item => res.json(item));
+});
+
+// Delete an entry
+app.delete('/:id', (req, res) => {
+  Animal.findOneAndDelete({ _id: req.params.id })
+    .then(() => res.json({ success: true }))
+    .catch(err => res.status(404).json({ success: false }));
+});
+// Update an entry
+app.put('/:id', (req, res) => {
+  Animal.findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(() => res.json({ success: true }))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port: http://localhost:${port}`));
